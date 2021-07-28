2484a1de0eb94347d8bfb3d3f3da54d96bcb8a3c
Erazem Kokot <contact@erazem.eu>
2021-07-28 16:31:11 +0200

    Update WSL install script

--- scripts/setup-fedora-wsl.sh
+++ scripts/setup-fedora-wsl.sh
@@ -9,70 +9,77 @@ GPG_PRIVKEY=default
 
 setup() {
     # Optimize DNF
+    echo "Optimizing DNF"
     echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
     echo "fastestmirror=True" >> /etc/dnf/dnf.conf
 
     # Update packages
-    dnf upgrade -y --refresh
+    echo "Upgrading packages"
+    dnf upgrade -y --quiet --refresh
 
     # Enable delta RPMs
-    dnf install -y deltarpm
+    echo "Enabling delta RPMs"
+    dnf install -y --quiet deltarpm
     echo "deltarpm=True" >> /etc/dnf/dnf.conf
-    dnf upgrade -y --refresh
+    dnf upgrade -y --quiet --refresh
 
     # Reinstall shadow-utils
-    dnf reinstall -y shadow-utils
+    echo "Reinstalling shadow utils"
+    dnf reinstall -y --quiet shadow-utils
 
     # Enable trustywolf/wslu COPR repo and install wslu
-    dnf copr enable -y trustywolf/wslu
-    dnf install -y wslu
+    echo "Enabling trustywolf/wslu COPR repo"
+    dnf copr enable -y --quiet trustywolf/wslu
+    dnf install -y --quiet wslu
 
     # Set root password
-    dnf install -y passwd cracklib-dicts
-    clear
+    dnf install -y --quiet passwd cracklib-dicts ncurses
     echo "Set root password:"
     passwd root
 }
 
 install_packages() {
-    dnf install -y $(grep "^[^#]" $PACKAGES)
+    echo "Installing selected packages: $(grep "^[^#]" $PACKAGES)"
+    dnf install -y --quiet $(grep "^[^#]" $PACKAGES)
 }
 
-cleanup() {
-    dnf autoremove -y
-    dnf clean all -y
+configure_sudo() {
+    # Allow the user to manually edit the sudoers file
+    echo "Edit the sudoers file (press enter to continue)"
+    read
+    visudo
 }
 
 setup_user() {
     # Install fish shell
-    dnf install -y fish
+    dnf install -y --quiet fish
 
-    clear
-    echo "Specify custom user's username:"
+    echo -n "Specify custom user's username: "
     read USER
-    echo "Specify GPG privkey file:"
+    echo -n "Specify GPG privkey file: "
     read GPG_PRIVKEY
-    echo "Specify GPG privkey email:"
+    echo -n "Specify GPG privkey email: "
     read EMAIL
 
     useradd -s /usr/bin/fish -G wheel $USER
-    clear
-    echo "Set password for $USER:"
+    echo -n "Set password for $USER: "
     passwd $USER
 }
 
 configure_dotfiles() {
-    dnf install -y git git-lfs
+    echo "Cloning dotfiles repo"
+    dnf install -y --quiet git git-lfs
     su $USER -c "
         mkdir -p /home/$USER/.config;
-        git clone https://gitlab.com/erazemk/dotfiles.git /home/$USER/.config/dotfiles;
-        git -C /home/$USER/.config/dotfiles remote set-url origin git@gitlab.com:erazemk/dotfiles.git
+        git clone --quiet https://gitlab.com/erazemk/dotfiles.git /home/$USER/.config/dotfiles;
+        git --quiet -C /home/$USER/.config/dotfiles remote set-url origin git@gitlab.com:erazemk/dotfiles.git
     "
 }
 
 run_stow() {
     # Set up dotfiles
-    dnf install -y stow
+    echo "Setting up dotfiles"
+    dnf install -y --quiet stow
 
     cd /home/$USER/.config/dotfiles
     su $USER -c "
@@ -106,20 +113,11 @@ run_stow() {
     "
 }
 
-configure_sudo() {
-    clear
-
-    # Allow the user to manually edit the sudoers file
-    echo "Edit the sudoers file (press enter to continue)"
-    read
-    visudo
-}
-
 configure_gpg() {
-    dnf install -y pinentry
+    echo "Setting up GPG key"
+    dnf install -y --quiet pinentry
     su $USER -c "mkdir /home/$USER/.config/gnupg; gpg --import $GPG_PRIVKEY"
 
-    clear
     # Set GPG privkey trust
     echo "Set the GPG key's trust (press enter to continue)"
     read
@@ -127,6 +125,7 @@ configure_gpg() {
 }
 
 setup_gotop() {
+    echo "Installing gotop"
     curl -s https://api.github.com/repos/xxxserxxx/gotop/releases/latest \
             | grep "browser_download_url.*linux_amd64" \
             | cut -d : -f2,3 \
@@ -139,8 +138,18 @@ setup_gotop() {
     rm gotop_*_linux_amd64.tgz
 }
 
+cleanup() {
+    echo "Cleaning up"
+    dnf autoremove -y --quiet
+    dnf clean all -y --quiet
+
+    rm /home/$USER/.bash*
+}
+
+# Extra, not used
 setup_docker() {
     # Install docker repo
+    echo "Installing docker"
     dnf config-manager -y --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
     dnf install -y docker-ce docker-ce-cli containerd.io
     systemctl start docker
@@ -153,5 +162,6 @@ configure_sudo
 setup_user
 configure_dotfiles
 run_stow
+configure_gpg
 setup_gotop
 cleanup
