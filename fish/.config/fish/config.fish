e81d6a5ad399b866b4970bf2c17fee0abeefb101
Erazem Kokot <mail@erazem.eu>
2022-02-10 15:15:14 +0100

    Major fish config updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,10 +11,7 @@ set -U fish_greeting ""
 
 # Random
 abbr lsblk 'lsblk -f'
-abbr code 'codium'
-abbr c 'codium'
 abbr top 'gotop'
-abbr conf 'kak ~/.config/fish/config.fish'
 
 # Directories
 abbr mkdir 'mkdir -p'
@@ -28,16 +25,43 @@ abbr cp 'cp -Riv'
 abbr mv 'mv -iv'
 abbr rm 'rm -Iv'
 
+# Pager (less) colors
+set -x LESS_TERMCAP_mb (set_color brred)
+set -x LESS_TERMCAP_md (set_color brred)
+set -x LESS_TERMCAP_me (set_color normal)
+set -x LESS_TERMCAP_se (set_color normal)
+set -x LESS_TERMCAP_so (set_color -b blue bryellow)
+set -x LESS_TERMCAP_ue (set_color normal)
+set -x LESS_TERMCAP_us (set_color brgreen)
+
 #############
 # Functions #
 #############
+
+function conf
+    if test (count $argv) -eq 0
+        kak ~/.config/fish/config.fish
+    else if test (count $argv) -eq 1
+        # Common configs
+        if test $argv[1] = "git"
+            kak ~/.config/git/config
+        # General parsing
+        else if test -f ~/.config/"$argv[1]"
+            kak ~/.config/"$argv[1]"
+        else if test -d ~/.config/"$argv[1]"
+            echo "Argument has to be a file!"
+        end
+    else
+        echo "Usage: conf <config file> (e.g. conf git/config)"
+    end
+end
 
 function grep -w grep
     command grep --color=auto $argv
 end
 
 function rcp -w rsync
-    rsync -vrulpEh --stats --progress $argv
+    command rsync -vrulpEh --stats --progress $argv
 end
 
 function cdtmp
@@ -45,7 +69,11 @@ function cdtmp
 end
 
 function info
-    curl cheat.sh/$argv[1]
+    command curl cheat.sh/$argv[1]
+end
+
+function sha256sum -w sha256sum
+    command sha256sum --ignore-missing $argv
 end
 
 #############
@@ -57,8 +85,8 @@ set PATH $PATH $HOME/.local/bin
 
 # Global variables
 set -x EDITOR kak
-set -x PAGER kak-pager
-set -x MANPAGER kak-pager
+set -x PAGER less
+set -x MANPAGER less
 
 set -x XDG_CACHE_HOME $HOME/.cache
 set -x XDG_CONFIG_HOME $HOME/.config
@@ -73,7 +101,7 @@ if test -d $XDG_CONFIG_HOME/gnupg
 end
 
 # Go config
-if test -d /usr/local/go
+if type -sq go
 	set -x GOPATH $XDG_DATA_HOME/go
 	set PATH $PATH $GOPATH/bin
 	set PATH $PATH /usr/local/go/bin
@@ -93,9 +121,9 @@ if test (cat /etc/os-release | grep -m 1 "ID" | cut -d '=' -f2) = "fedora"
 end
 
 # WSL-specific config
-if set -q WSL_DISTRO_NAME
-    abbr dir 'cd /mnt/c/Users/Erazem'
-end
+#if set -q WSL_DISTRO_NAME
+#    abbr dir 'cd /mnt/c/Users/Erazem'
+#end
 
 # If flutter is installed, add its files to PATH
 if test -d $XDG_DATA_HOME/flutter
