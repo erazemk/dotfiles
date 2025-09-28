daeeae92aa7e44c2dc085b618b4b36a07e09b999
Erazem Kokot <mail@erazemk.com>
2025-09-28 16:27:58 +0200

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -14,13 +14,15 @@ if test -d /opt/homebrew
     ! set -q INFOPATH; and set INFOPATH ''
     set -gx INFOPATH $HOMEBREW_PREFIX/share/info $INFOPATH
 
-    abbr brewer 'brew update && brew upgrade --greedy && brew cleanup'
+    abbr brewer 'brew update && brew upgrade && brew autoremove && brew cleanup'
 end
 
 # Program variables
 set -x EDITOR hx
 set -x GIT_TERMINAL_PROMPT 1
 set -x GOPATH $XDG_DATA_HOME/go
+set -x GOPRIVATE github.com/devrev
+set -x HOWTO_AI_TOKEN (security find-generic-password -w -s nalgeon/howto -a nalgeon/howto)
 fish_add_path -gP $GOPATH/bin
 
 if status is-interactive
@@ -32,19 +34,32 @@ if status is-interactive
     set -x LESS_TERMCAP_md (set_color brred)
     set -x LESS_TERMCAP_me (set_color normal)
     set -x LESS_TERMCAP_se (set_color normal)
-    set -x LESS_TERMCAP_so (set_color -b blue bryellow)
     set -x LESS_TERMCAP_ue (set_color normal)
     set -x LESS_TERMCAP_us (set_color brgreen)
+    set -x LESS_TERMCAP_so (set_color -b blue bryellow)
 
     # Abbreviations
-    abbr mkdir 'mkdir -p'
-    abbr cp 'cp -Riv'
+    abbr g git
     abbr mv 'mv -iv'
     abbr rm 'rm -Iv'
+    abbr cp 'cp -Riv'
+    abbr mkdir 'mkdir -p'
     abbr cdtmp 'cd (mktemp -d)'
 
     # Get cheatsheets for programs
     function tldr
         command curl cheat.sh/"$argv[1]"
+    end
+
+    function jwt
+        echo "$argv[1]" | jq -R 'split(".") | .[1] | @base64d | fromjson'
+    end
+
+    function dr
+        if ! command -v devrev &>/dev/null
+           go install -v github.com/devrev/devrev-cli/devrev@main
+        end
+
+        command devrev -q $argv
     end
 end
