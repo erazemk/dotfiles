857531480bc7e02de318232d58cae40ec75960d7
Erazem Kokot <mail@erazemk.com>
2026-04-24 19:25:44 +0200

    Update the dotfiles setup script

--- setup.sh
+++ setup.sh
@@ -2,36 +2,64 @@
 
 DOTFILES="$HOME/.config/dotfiles"
 
-mkdir -p ~/.config/fish
-ln -s $DOTFILES/git ~/.config/
-ln -s $DOTFILES/fish/config.fish ~/.config/fish/
+#
+# Copy dotfiles
+#
 
-# macOS-specific
-if [[ $(uname) == "Darwin" ]]; then
-    mkdir -p ~/.aws ~/.colima/_templates ~/Library/Application\ Support/Code/User/
-    ln -s $DOTFILES/aws/config ~/.aws/
-    ln -s $DOTFILES/aerospace ~/.config/
-    ln -s $DOTFILES/fish/conf.d ~/.config/fish/
-    ln -s $DOTFILES/vscode/settings.json $DOTFILES/vscode/keybindings.json ~/Library/Application\ Support/Code/User/
+# Aerospace
+echo "Setting up Aerospace dotfiles"
+mkdir -p ~/.config/aerospace
+ln -sf $DOTFILES/aerospace/* ~/.config/aerospace/
 
-    # Homebrew
-    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    brew install -y awscli colima curl docker docker-buildx docker-compose docker-credential-helper fish gh git git-lfs go helix jq node openssh python ripgrep tree unzip uv xz zstd
-    brew install -y --cask aerospace aws-vpn-client brave-browser codex ghostty lookaway monitorcontrol pearcleaner slack visual-studio-code
+# AWS
+echo "Setting up AWS dotfiles"
+mkdir -p ~/.aws
+ln -sf $DOTFILES/aws/* ~/.aws/
 
-    # Switch to fish shell
-    sudo chsh -s $(which fish) $(id -un)
-    sudo chsh -s $(which fish) root
+# Colima
+echo "Setting up Colima dotfiles"
+mkdir -p ~/.config/colima/_templates ~/Library/LaunchAgents
+ln -sf $DOTFILES/colima/template.yml ~/.config/colima/_templates/default.yml
+ln -sf $DOTFILES/launchctl/com.erazemk.colima.plist ~/Library/LaunchAgents/
+launchctl load ~/Library/LaunchAgents/com.erazemk.colima.plist
 
-    ln -s $DOTFILES/colima/template.yml ~/.colima/_templates/default.yml
-    brew services start colima
-fi
+# Fish
+echo "Setting up Fish dotfiles"
+mkdir -p ~/.config/fish†
+ln -sf $DOTFILES/fish/* ~/.config/fish/
 
-# Coding agents
-mkdir ~/.pi/agent ~/.agents ~/.claude ~/.codex
-ln -s $DOTFILES/pi/extensions $DOTFILES/pi/prompts ~/.pi/agent/
-ln -s $DOTFILES/pi/skills ~/.agents/
-ln -s $DOTFILES/pi/skills $DOTFILES/claude-code/settings.json ~/.claude/
-ln -s $DOTFILES/pi/AGENTS.md ~/.pi/agent/
-ln -s $DOTFILES/pi/AGENTS.md ~/.claude/CLAUDE.md
-ln -s $DOTFILES/pi/AGENTS.md ~/.codex/
+# Git
+echo "Setting up Git dotfiles"
+mkdir -p ~/.config/git
+ln -sf $DOTFILES/git/* ~/.config/git/
+
+# OpenCode
+echo "Setting up OpenCode dotfiles"
+mkdir -p ~/.config/opencode
+ln -sf $DOTFILES/opencode/* ~/.config/opencode/
+ln -sf $DOTFILES/agents/* ~/.config/opencode/
+
+# VSCode
+echo "Setting up VSCode dotfiles"
+mkdir -p ~/Library/Application\ Support/Code/User/
+ln -sf $DOTFILES/vscode/* ~/Library/Application\ Support/Code/User/
+
+# Other
+echo "Setting up other dotfiles"
+mkdir -p ~/.local/bin
+
+#
+# Installation steps
+#
+
+# Homebrew
+echo "Installing Homebrew"
+sudo -v
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install awscli colima docker docker-buildx docker-compose docker-credential-helper fish gh git git-lfs go helix jq anomalyco/tap/opencode
+brew install --cask aws-vpn-client bitwarden brave-browser ghostty granola handy lookaway obsidian pearcleaner slack visual-studio-code nikitabobko/tap/aerospace
+
+# Switch to fish shell
+echo "Switching to fish shell"
+sudo chsh -s $(which fish) $(id -un)
+sudo chsh -s $(which fish) root
